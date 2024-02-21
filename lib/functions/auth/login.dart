bdfe41ee0ee19edd09888b9e3278f080249c8ece
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-02-22 02:31:29 +0300

    ...login

--- lib/functions/auth/login.dart
+++ lib/functions/auth/login.dart
@@ -1,17 +1,11 @@
-
 import 'package:mysql_client/mysql_client.dart';
 
-Future<Map<String, dynamic>> authUser(String email, String password, MySQLConnection sql) async {
-  final response = await sql.execute(
-    "SELECT * FROM users",
-    {},
-  );
-  for (final row in response.rows) {
-    var data = row.assoc();
-    if (data['email'] == email && data['password_hast'] == password) {
-      return {'success': true, 'uid': row.assoc()['id']};
-    }
-    print(row.assoc());
-  }
+Future<Map<String, dynamic>> authUser(
+    String email, String password, MySQLConnection sql) async {
+
+  try {
+    final user = await sql.execute("SELECT * FROM users where email = '$email' and password_hast = '$password", {}, );
+    print(user.rows.first.assoc()['name']);
+  } catch (e) {}
   return {'success': false};
 }
